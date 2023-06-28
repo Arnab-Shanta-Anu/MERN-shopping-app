d8e1106082ab27c765dba6737d870294cdcb62af
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-28 20:06:32 +0600

    deploy

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//serve frontend
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/dist/index.html")),
+    (error) => {
+      res.status(500).send(error);
+    };
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
