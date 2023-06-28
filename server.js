cd22f3902795772c1707549a0d0e4f341023f7bd
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-28 17:47:59 +0600

    server.js changed

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World!");
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
