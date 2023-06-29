585592f78eb88c32df6806eb1a7cc1c39ce8501f
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-29 14:37:23 +0600

    redirect all to the frontend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 //serve frontend
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")),
     (error) => {
       res.status(500).send(error);
